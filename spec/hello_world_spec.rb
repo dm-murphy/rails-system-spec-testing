f2429b43e30382b1c9534a8c210df38666b4e9bb
Daniel Murphy <danielmichaelmurphy@gmail.com>
2022-01-07 14:56:59 -0500

    Add sleep to index path test

--- spec/hello_world_spec.rb
+++ spec/hello_world_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe 'Hello world', type: :system do
   describe 'index page' do
     it 'shows the right content' do
       visit hello_world_index_path
+      sleep(5)
       expect(page).to have_content('Hello, world!')
     end
   end
