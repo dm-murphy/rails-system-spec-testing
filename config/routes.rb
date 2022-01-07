3b88b56e13bd5ca2faa0031e0d9c9d3c2561c61f
Daniel Murphy <danielmichaelmurphy@gmail.com>
2022-01-07 15:24:47 -0500

    Generate devise User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'hello_world/index'
   root "hello_world#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
