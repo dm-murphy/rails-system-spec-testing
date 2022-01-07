494c33c89732e5c5f224b8ff021a537abcc834c3
Daniel Murphy <danielmichaelmurphy@gmail.com>
2022-01-07 16:20:13 -0500

    Configure Capybara for Devise gem

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -31,6 +31,8 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 RSpec.configure do |config|
+  # Configure Capybara for Devise gem
+  config.include Warden::Test::Helpers
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
